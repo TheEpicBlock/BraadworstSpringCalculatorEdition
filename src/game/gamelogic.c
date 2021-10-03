bba173c1c21f190fbf40ea7851fa6757cab0ac85
TheEpicBlock <git.teb@theepicblock.nl>
2021-10-03 21:54:23 +0200

    Switch to timer 1, hopefully fixing stuff

--- src/game/gamelogic.c
+++ src/game/gamelogic.c
@@ -11,7 +11,7 @@ const int OBSTACLE_MAX_OFFSET = 5;
 const int OBSTACLE_SPEED = 80;
 
 const int SECOND = 32768;
-const int TIMER = 2;
+const int TIMER = 1;
 
 static float Max(float a, float b) {
     if (a > b) {
