571fcf4ee2c74f1632d78b30a959c5720832fb45
TheEpicBlock <git.teb@theepicblock.nl>
2021-10-03 19:53:30 +0200

    Spawn new obstacles once they're out of the screen

--- src/game/gamelogic.c
+++ src/game/gamelogic.c
@@ -3,9 +3,9 @@
 
 #include "game/gamelogic.h"
 #include "rendering/renderer.h"
+#include "gfx/generated/gfx.h"
 
-
-const int OBSTACLE_SPACING = LCD_WIDTH * 0.75f;
+const int OBSTACLE_SPACING = LCD_WIDTH * 0.9f;
 const int OBSTACLE_MAX_OFFSET = 5;
 const int OBSTACLE_SPEED = 80;
 
@@ -21,14 +21,18 @@ static float Max(float a, float b) {
 }
 
 int GetNewPositionOffset(void) {
-    return 0;
+    return OBSTACLE_SPACING + 0;
 }
 
 void game_InitState(struct GameState *state) {
+    // Initialize obstacles
+    float pos = GetNewPositionOffset();
     for (int i = 0; i < OBSTACLE_COUNT; i += 1) {
-        state->obstacles[i].position = i*OBSTACLE_SPACING + GetNewPositionOffset();
+        state->obstacles[i].position = pos;
+        pos += GetNewPositionOffset();
     }
-    
+
+    // Initialize timers    
     timer_Set(TIMER, 0);
     timer_Enable(TIMER, TIMER_32K, TIMER_NOINT, TIMER_UP);
 }
@@ -50,6 +54,9 @@ bool game_Tick(struct GameState *state) {
     // Move obstacles
     for (int i = 0; i < OBSTACLE_COUNT; i += 1) {
         state->obstacles[i].position -= OBSTACLE_SPEED * delta;
+        if (state->obstacles[i].position < -genericobstacle_width) {
+            state->obstacles[i].position = state->obstacles[OBSTACLE_COUNT-1].position + GetNewPositionOffset();
+        }
     }
     
     // Jumping
